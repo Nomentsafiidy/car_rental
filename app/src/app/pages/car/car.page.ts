8e072d445ccec1f8a4fe2024c3b1152c2438c6fd
Nomentsafidy <mnomentsafidy@gmail.com>
2021-06-06 11:31:40 +0300

    feat delete car

--- app/src/app/pages/car/car.page.ts
+++ app/src/app/pages/car/car.page.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Car } from 'src/app/models/car';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
     selector: 'app-car',
@@ -12,7 +14,7 @@ export class CarPage implements OnInit {
     public cars: Car[] = [];
     private keyWord: string = '';
 
-    constructor(private httpClient: HttpClient) {}
+    constructor(private httpClient: HttpClient, public alertController: AlertController, public toastController: ToastController) {}
 
     ngOnInit() {
         this.fetchData(this.keyWord);
@@ -26,6 +28,34 @@ export class CarPage implements OnInit {
             .subscribe((res: any) => {
                 this.cars = res.cars;
             });
+    }
+
+    async onDelete(car: Car) {
+        const alert = await this.alertController.create({
+            cssClass: 'my-custom-class',
+            header: 'Confirm!',
+            message: 'Voulez supprime <strong>' + car.designation + '</strong>!!!',
+            buttons: [
+                'Annuler',
+                {
+                    text: 'Oui',
+                    handler: () => {
+                        this.httpClient.get(environment.apiUrl + 'deleteCar/' + car.id).subscribe(async (res: any) => {
+                            console.log('res delete', res);
+                            const toast = await this.toastController.create({
+                                message: 'Voiture supprimé',
+                                duration: 2000,
+                                color: 'success',
+                            });
+                            toast.present();
+                            this.fetchData(this.keyWord);
+                        });
+                    },
+                },
+            ],
+        });
+
+        await alert.present();
     }
 
     keyWordChange(event: any) {
